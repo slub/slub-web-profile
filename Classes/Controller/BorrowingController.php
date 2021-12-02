ae4b25417f9581e4ab9e6fa297314689f6fc7fef
Daniela Schmidt <daniela.schmidt@e-pixler.com>
2021-12-02 18:14:55 +0100

    [FEATURE] #6 borrowinglist

---
+++ Classes/Controller/BorrowingController.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the package slub/slub-web-profile
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace Slub\SlubWebProfile\Controller;
+
+use Slub\SlubWebProfile\Service\EventService;
+use Slub\SlubWebProfile\Utility\FrontendUserUtility;
+use Slub\SlubWebProfile\Utility\LanguageUtility;
+use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+
+class BorrowingController extends ActionController
+{
+    public function listAction(): void
+    {
+
+    }
+}
