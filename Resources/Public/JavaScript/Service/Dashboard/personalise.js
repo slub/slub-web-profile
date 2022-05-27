935effad8241ad3fb81d2677ed163d8ddb130778
Daniela Schmidt <daniela.schmidt@e-pixler.com>
2022-05-27 15:50:58 +0200

    [TASK] Controller styling for personalise dashboard

---
+++ Resources/Public/JavaScript/Service/Dashboard/personalise.js
@@ -0,0 +1,65 @@
+/**
+ * @type {string}
+ */
+const isOpenClass = 'personalise--is-open';
+
+/**
+ * @type {string}
+ */
+const containerSelector = '#js-personalise';
+
+/**
+ * @type {string}
+ */
+const headerSelector = '#js-personalise-header';
+
+/**
+ * @type {string}
+ */
+const closeSelector = '#js-personalise-close';
+
+/**
+ * @type {string}
+ */
+const controllerSelector = '#js-personalise-controller';
+
+/**
+ * @type {Element}
+ */
+const containerElement = document.querySelector(containerSelector);
+
+/**
+ * @type {Element}
+ */
+const headerElement = document.querySelector(headerSelector);
+
+/**
+ * @type {Element}
+ */
+const closeElement = document.querySelector(closeSelector);
+
+/**
+ * @type {Element}
+ */
+const controllerElement = document.querySelector(controllerSelector);
+
+export const listenHeader = () => headerElement.addEventListener('click', () => toggleItem());
+export const listenClose = () => closeElement.addEventListener('click', () => toggleItem());
+
+const toggleItem = () => {
+  containerElement.classList.toggle(isOpenClass);
+
+  if (containerElement.classList.contains(isOpenClass)) {
+    headerElement.setAttribute('aria-expanded', 'true');
+    closeElement.setAttribute('aria-hidden', 'false');
+    controllerElement.setAttribute('aria-hidden', 'false');
+  } else {
+    headerElement.setAttribute('aria-expanded', 'false');
+    closeElement.setAttribute('aria-hidden', 'true');
+    controllerElement.setAttribute('aria-hidden', 'true');
+  }
+}
+
+// No external call, call the "export" direct
+listenHeader();
+listenClose();
--- Resources/Public/JavaScript/dashboard.js
+++ Resources/Public/JavaScript/dashboard.js
@@ -1,5 +1,6 @@
 import * as widget from './Service/Dashboard/widget.js';
 import * as controller from './Service/Dashboard/controller.js';
+import * as personalise from './Service/Dashboard/personalise.js';
 
 widget.initialize();
 controller.initialize();
